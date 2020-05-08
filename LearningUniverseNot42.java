17c7acf7cd4817f6c6d162810b5a0d3f08224b74
Prakhar Indoria <52858437+prakharindoria@users.noreply.github.com>
2020-05-08 23:53:17 +0530

    +Problem Statement

--- LearningUniverseNot42.java
+++ LearningUniverseNot42.java
@@ -5,6 +5,28 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
+
+/*Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. 
+More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. 
+All numbers at input are integers of one or two digits.
+
+Example
+Input:
+1
+2
+88
+42
+99
+
+Output:
+1
+2
+88
+
+Time Limit:	10 secs
+Source Limit:	50000 Bytes
+
+*/
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
