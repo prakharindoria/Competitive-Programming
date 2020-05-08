d085c6e2bcf960270ebd41ffa5448557022dfccf
Prakhar Indoria <52858437+prakharindoria@users.noreply.github.com>
2020-05-08 23:49:16 +0530

    Learning Universe On Codechef.
    Loops

---
+++ 32881763.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner kb=new Scanner(System.in);
+		int a=0;
+		while(a!=42)
+		{   
+		    a=kb.nextInt();
+		    if(a!=42)
+		    System.out.println(a);
+     	}
+	}
+}
