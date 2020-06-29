2e43cb12d66e5b5dd27595f280f37c9e1ec87f16
Prakhar Indoria <prakharindoria@gmail.com>
2020-06-30 00:08:02 +0530

    Update DictionaryAndMaps8.java

--- DictionaryAndMaps8.java
+++ DictionaryAndMaps8.java
@@ -53,21 +53,32 @@ Query 2: harry
 Harry is one of the keys in our dictionary, so we print harry=12299933.*/
 
 //Complete this code or write your own from scratch
+//Complete this code or write your own from scratch
 import java.util.*;
 import java.io.*;
 
 class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
+        
+        Map<String,Integer> hm=new HashMap<String,Integer>();
+
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
-            // Write code here
+            hm.put(name,phone);
+            
         }
         while(in.hasNext()){
             String s = in.next();
             // Write code here
+            int res;
+            if(hm.containsKey(s))
+            System.out.println(s+"="+hm.get(s));
+            else
+            System.out.println("Not found");
+
         }
         in.close();
     }
