a99133fe236798b17621178e7fdc336d57ccae57
Prakhar Indoria <prakharindoria@gmail.com>
2020-07-24 23:49:37 +0530

    Create HrInterfaces.java

---
+++ HrInterfaces.java
@@ -0,0 +1,69 @@
+/*HR
+Objective
+Today, we're learning about Interfaces. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
+
+Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum(n) method must return the sum of all divisors of .
+
+Input Format
+
+A single line containing an integer, 
+Output Format
+
+You are not responsible for printing anything to stdout. The locked template code in the editor below will call your code and print the necessary output.
+
+Sample Input
+
+6
+Sample Output
+
+I implemented: AdvancedArithmetic
+12
+Explanation
+
+The integer  is evenly divisible by , , , and . Our divisorSum method should return the sum of these numbers, which is . The Solution class then prints  on the first line, followed by the sum returned by divisorSum (which is ) on the second line.
+
+*/
+
+
+
+class Calculator{
+    int power(int n,int p) throws Xception{
+            if(n<0 || p<0)
+            throw new Xception("n and p should be non-negative");
+                return (int)Math.pow(n,p);
+                 }
+    }
+
+class Xception extends Exception{
+    Xception(String s) 
+    {  
+        super(s); 
+    } 
+
+}
+
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
