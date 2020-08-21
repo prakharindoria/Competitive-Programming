eeae23c8a06883c6e8acf8e865e6dbc9c27a391b
Prakhar Indoria <prakharindoria@gmail.com>
2020-08-21 23:53:47 +0530

    Create HeightOfBinaryTree.java

---
+++ HeightOfBinaryTree.java
@@ -0,0 +1,34 @@
+/*
+Height of Binary Tree 
+Given a binary tree, find its height.
+
+​​Example 1:
+
+Input:
+      1
+    /  \
+   2    3
+Output: 2
+Example 2:
+
+Input:
+  2
+   \
+    1
+   /
+ 3
+Output: 3   
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function height() that takes root Node of the Tree as input and returns the Height of the Tree. If the Tree is empty, return 0. 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+
+ 
+
+** For More Input/Output Examples Use 'Expected Output' option **
+*/
