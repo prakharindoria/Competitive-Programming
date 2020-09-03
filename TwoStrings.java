5cf3c26da9c70a3870a58c1995e6b4789fa18b89
Prakhar Indoria <prakharindoria@gmail.com>
2020-09-03 23:28:34 +0530

    Update TwoStrings.java

--- TwoStrings.java
+++ TwoStrings.java
@@ -95,3 +95,78 @@ public class Solution {
         scanner.close();
     }
 }
+
+//Highly Scalable Code. Can handle inputs even in millions.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        boolean res=false;
+        HashSet<Character> hm=new HashSet<>();
+        HashSet<Character> hm2=new HashSet<>();
+
+        for(int i=0;i<s1.length();i++){
+            hm.add(s1.charAt(i));
+            if(hm.size()>26)
+            break;
+        }
+        
+        for(int i=0;i<s2.length();i++){
+            hm2.add(s2.charAt(i));
+            if(hm2.size()>26)
+            break;
+        }
+
+        for(Character ch1: hm){
+                for(Character ch2:hm2 ){
+                if(ch1==ch2){res=true;break;}
+            }
+            if(res==true)
+            break;
+        }
+
+        if(res==true)
+        return "YES";
+        else
+        return "NO";
+
+        
+        
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+
