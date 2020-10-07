a04e3bfb21ad5e96dc902f9300e56d7355081f8c
Prakhar Indoria <prakharindoria@gmail.com>
2020-10-07 23:55:24 +0530

    Create BinaryToDecimal.java

---
+++ BinaryToDecimal.java
@@ -0,0 +1,25 @@
+/*
+Binary to Decimal
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int bin=111;//Binary Number 
+		int nod=0;
+		int cbin=bin;
+		while(cbin>0){
+		    nod=nod+1;
+		    cbin=cbin/10;
+		}
+		
+		int res=0;
+		
+		for(int i=0;i<=nod;i++){
+		   int temp=bin%10;
+		    res+=(Math.pow(2,i)*temp);
+		    bin=bin/10;
+		}
+		System.out.println(res);
+	}
+}
