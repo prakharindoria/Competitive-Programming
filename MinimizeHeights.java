b1335286541971989ede71d6cc10fc651c8ee427
Prakhar Indoria <prakharindoria@gmail.com>
2020-10-15 00:39:28 +0530

    Update MinimizeHeights.java

--- MinimizeHeights.java
+++ MinimizeHeights.java
@@ -1,4 +1,5 @@
-Given an array arr[] denoting heights of N towers and a positive integer K, modify the heights of each tower either by increasing or decreasing them by K only once. Find out the minimum difference of the heights of shortest and longest modified towers.
+
+/*Given an array arr[] denoting heights of N towers and a positive integer K, modify the heights of each tower either by increasing or decreasing them by K only once. Find out the minimum difference of the heights of shortest and longest modified towers.
 
 Example 1:
 
@@ -31,3 +32,72 @@ Constraints
 1 <= K <= 104
 1 <= N <= 105
 1 <= Arr[i] <= 105
+
+  
+  */
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(inputLine[0]);
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().getMinDiff(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    int getMinDiff(int[] arr, int n, int k) {
+        
+        
+        if(n==1)
+        return 0;
+        
+            Arrays.sort(arr);
+            int s=arr[0]+k;
+            int h=arr[n-1]-k;
+            int temp=0;
+            int ans=arr[n-1]-arr[0];
+            
+            if(s>h){temp=s;s=h;h=temp;}//swap
+            
+            
+            for(int i=1;i<n-1;i++){
+                int cs=arr[i]-k;
+                int ch=arr[i]+k;
+                
+                if(cs>=s || ch<=h)continue;
+                
+                
+                if(h-cs <= ch-s)s=cs;
+                else h=ch;
+                
+                
+            }
+            
+            
+            return Math.min(ans,h-s);
+            
+            
+    }
+}
