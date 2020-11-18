04bfa771782b9d2a39f0b5f31cb9d3b5bf04fce0
Prakhar Indoria <prakharindoria@gmail.com>
2020-11-18 23:54:54 +0530

    Create PreOrder.java

---
+++ PreOrder.java
@@ -0,0 +1,33 @@
+/*
+Complete the  function in your editor below, which has  parameter: a pointer to the root of a binary tree. It must print the values in the tree's preorder traversal as a single line of space-separated values.
+
+Input Format
+
+Our hidden tester code passes the root node of a binary tree to your preOrder function.
+
+Constraints
+
+ Nodes in the tree 
+
+Output Format
+
+Print the tree's preorder traversal as a single line of space-separated values.
+
+Sample Input
+
+     1
+      \
+       2
+        \
+         5
+        /  \
+       3    6
+        \
+         4  
+Sample Output
+
+1 2 5 3 4 6 
+Explanation
+
+The required result of the preorder traversal of the given binary tree is as above.
+*/
