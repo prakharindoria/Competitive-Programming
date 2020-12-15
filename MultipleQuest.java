b09f37bd59c5d8e27dfe2e7aae75d97e11d54450
Prakhar Indoria <prakharindoria@gmail.com>
2020-12-16 00:03:41 +0530

    Update MultipleQuest.java

--- MultipleQuest.java
+++ MultipleQuest.java
@@ -3,3 +3,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 */
+public class Solution {
+
+  public int solution(int number) {
+    int res=0;
+    int sum=0;
+    for(int i=1;i<number;i++){
+      if(i%15==0)sum+=i;
+      else if(i%5==0)sum+=i;
+      else if(i%3==0)sum+=i;
+    }
+    res=sum;
+    return res;
+  }
+}
