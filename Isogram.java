53c95074b8bfb70d3721eac5e9d50efe749699c4
Prakhar Indoria <prakharindoria@gmail.com>
2020-12-18 23:41:12 +0530

    Update Isogram.java

--- Isogram.java
+++ Isogram.java
@@ -6,3 +6,22 @@ isIsogram "aba" == false
 isIsogram "moOse" == false -- ignore letter case
 
 */
+public class isogram {
+    public static boolean  isIsogram(String str) {
+      boolean flag=true;
+      str=str.toLowerCase();
+      if(str.length()==0)return true;
+      else{
+        for(int i=0;i<str.length();i++){
+          for(int j=i+1;j<str.length();j++){
+            if(str.charAt(i)==str.charAt(j)){flag=false;break;}
+          }
+          if(flag==false)break;
+        }
+      if(flag==true)return true;
+      else return false;
+        }
+      
+      
+    } 
+}
