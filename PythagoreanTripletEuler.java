3ef553cd1d11686888fe924a034043a8fb055150
Prakhar Indoria <prakharindoria@gmail.com>
2020-12-23 00:40:38 +0530

    Update PythagoreanTripletEuler.java

--- PythagoreanTripletEuler.java
+++ PythagoreanTripletEuler.java
@@ -7,3 +7,27 @@ For example, 32 + 42 = 9 + 16 = 25 = 52.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
+public class PythagoreanTripletV2 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        int product = 0;
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 500; j++) {
+                int k = 1000 - (i + j);
+                if (i + j + k == 1000) {
+                    int lhs = (j * j) + (k * k);
+                    int rhs = i * i;
+                    if (lhs == rhs) {
+                        flag = true;
+                        product = i * j * k;
+                    }
+                }
+                if (flag == true) break;
+            }
+            if (flag == true) break;
+            }
+            System.out.println(product);
+
+        }
+    }
+
