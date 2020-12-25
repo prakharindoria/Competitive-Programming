aab156e23ce76ab4a779120ffb5ad1103645519f
Prakhar Indoria <prakharindoria@gmail.com>
2020-12-25 23:22:34 +0530

    Update PowerDigitEuler.java

--- PowerDigitEuler.java
+++ PowerDigitEuler.java
@@ -4,3 +4,22 @@
 What is the sum of the digits of the number 21000?
 
 */
+
+import java.math.BigInteger;
+public class PowerDigitSum {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 1000;
+        BigInteger numa = new BigInteger(String.valueOf(a));
+        BigInteger num= numa.pow(b);
+        BigInteger sum=new BigInteger(""+0);
+        BigInteger limit= new BigInteger(""+0);
+        while(num.compareTo(limit)>0){
+            BigInteger ld=num.mod(new BigInteger(""+10));
+            sum=sum.add(ld);
+            num=num.divide(new BigInteger(""+10));;
+        }
+        System.out.println(sum);
+    }
+}
+
