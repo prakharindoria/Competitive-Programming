2694334db1d782c0947f9ea946105bc2a71491cf
Prakhar Indoria <prakharindoria@gmail.com>
2020-12-28 23:58:18 +0530

    Update FactorialSumEuler.java

--- FactorialSumEuler.java
+++ FactorialSumEuler.java
@@ -6,3 +6,29 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
 */
+import java.math.BigInteger;
+
+public class FactorialSum {
+    public static void main(String[] args) {
+
+        BigInteger bn=new BigInteger(""+factorial(100));
+        BigInteger bs=new BigInteger(""+0);
+        BigInteger zero=new BigInteger(""+0);
+
+        while(bn.compareTo(zero)>0){
+            BigInteger ld=bn.mod(new BigInteger(""+10));
+            bs=bs.add(ld);
+            bn=bn.divide(new BigInteger(""+10));
+        }
+        System.out.println(bs);
+    }
+    public static BigInteger factorial(int n){
+        BigInteger res=new BigInteger(""+1);
+        for (int i = 1; i <= n; i++) {
+            res=res.multiply(new BigInteger(""+i));
+        }
+        return res;
+
+
+    }
+}
