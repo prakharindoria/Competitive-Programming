02205a7c118ce5411030e02ec8a4b5afbaf54eaa
Prakhar Indoria <prakharindoria@gmail.com>
2021-01-09 21:56:49 +0530

    Update NthFibonacciDigitEuler.java

--- NthFibonacciDigitEuler.java
+++ NthFibonacciDigitEuler.java
@@ -53,3 +53,48 @@ public class NthThousandDigitFibonacci {
 
 
 */
+
+//Solution 
+
+import java.math.BigInteger;
+public class NthThousandDigitFibonacci 
+{
+    public static void main(String[] args) 
+    {
+        BigInteger a=new BigInteger("0");
+        BigInteger b=new BigInteger("1");
+        BigInteger c=new BigInteger("0");
+        int index=0;
+        //Itrating till 1000th digit number.
+        while(nod(c).compareTo(new BigInteger("1000"))<0)
+        {
+            a=b;
+            b=c;
+            c=a.add(b);
+            index++;
+        }
+        System.out.println(index);
+    }
+    
+    
+    /*Method to calculate Number of Digits.
+    nod:numberOfDigits
+    */
+    public static BigInteger nod(BigInteger n)
+    {
+        BigInteger res=new BigInteger("0");
+       
+        while(n.compareTo(new BigInteger("0"))>0)
+        {
+            BigInteger ld=n.mod(new BigInteger("10"));
+            res=res.add(new BigInteger("1"));
+            n=n.divide(new BigInteger("10"));
+        }
+        return res;
+    }
+
+}
+
+
+
+
