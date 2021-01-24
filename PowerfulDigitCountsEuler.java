7efd0be689afde5ee1725ded37f064753424bcda
Prakhar Indoria <prakharindoria@gmail.com>
2021-01-24 23:46:21 +0530

    Update PowerfulDigitCountsEuler.java

--- PowerfulDigitCountsEuler.java
+++ PowerfulDigitCountsEuler.java
@@ -3,3 +3,22 @@ The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit numb
 
 How many n-digit positive integers exist which are also an nth power?
 */
+public class PowerfulDigitCounts {
+    public static void main(String[] args) {
+
+        int count=0;
+        for (double n = 0; n < 300; n++) {
+        double minNdigitNum= Math.pow(10,n-1);
+        double minNPlusdigitNum=  Math.pow(10,n);
+        double temp=0;
+        int i=1;
+        while(temp<minNPlusdigitNum){
+            temp= Math.pow(i,n);
+            if(temp>=minNdigitNum && temp<minNPlusdigitNum)
+            {count++;System.out.println(temp);}
+            i++;
+        }
+        }
+        System.out.println(count);
+    }
+}
