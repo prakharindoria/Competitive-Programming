6a4fdee1045636f896ba4b9a4bedd77ce89c7c2d
Prakhar Indoria <prakharindoria@gmail.com>
2021-01-26 23:41:46 +0530

    Update ConsecutivePrimeSumEuler.java

--- ConsecutivePrimeSumEuler.java
+++ ConsecutivePrimeSumEuler.java
@@ -9,3 +9,30 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
 */
+
+
+public class ConsecutivePrimeSum {
+    public static void main(String[] args) {
+        int MAX_SUM = 1000000;
+        int currentSum = 0;
+        int currentNumber = 6;
+
+        while ((currentSum + currentNumber) < MAX_SUM) {
+            if (isPrime(currentNumber)) {
+                currentSum += currentNumber;
+            }
+            currentNumber++;
+        }
+        System.out.println(currentSum);
+    }
+
+    public static boolean isPrime(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if (n % i == 0 && n != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
